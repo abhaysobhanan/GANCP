8ad98eae391af462eddebc606125f4e132cfcbc7
Abhay Sobhanan <sobhanan@usf.edu>
2024-05-01 06:49:11 -0400

    remove code comment

--- src/evaluate_sol.jl
+++ src/evaluate_sol.jl
@@ -16,8 +16,6 @@ from src.dgl.get_graph import get_knn_graph
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = Transformer(in_dim=4, latent_dim=128, n_layers=4).to(device)
-# state_dict = torch.load($py_dir+'weights/model_step4.pt',
-#                         map_location=torch.device(device))
 state_dict = torch.load(os.path.join($py_dir, 'weights', 'model_step4.pt'),
                         map_location=torch.device(device))
 model.load_state_dict(state_dict)
